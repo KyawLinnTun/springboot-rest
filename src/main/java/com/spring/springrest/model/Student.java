c656bba869cc638a349c7ed0bd87b4bd08bfc621
Kyaw Lin Tun <kyawlin.klt4991@gmail.com>
2020-08-14 16:24:36 +0630

    commit

--- src/main/java/com/spring/springrest/model/Student.java
+++ src/main/java/com/spring/springrest/model/Student.java
@@ -14,9 +14,7 @@ public class Student implements Serializable{
     private int id;
     private String name;
     private String phone;
-   
-    
-
+    private String address;
     private String email;
 
     private String school;
