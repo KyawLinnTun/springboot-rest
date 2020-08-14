6c8c168d99a5d681ca3f688d429fc51505cab8ee
KyawLinnTun <36469560+KyawLinnTun@users.noreply.github.com>
2020-08-14 16:19:52 +0630

    Update Student.java

--- src/main/java/com/spring/springrest/model/Student.java
+++ src/main/java/com/spring/springrest/model/Student.java
@@ -14,7 +14,8 @@ public class Student implements Serializable{
     private int id;
     private String name;
     private String phone;
-    private String address;
+   
+    
 
     private String email;
 
